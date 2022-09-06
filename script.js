48d51f9d893351fdc28377b258e952012c4bf616
Federico Garcia <federicoignaciogarcia@gmail.com>
2022-09-06 01:59:29 -0300

    Dark Mode implementado con variables CSS

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+var main = document.getElementById("main");
 
 // Scroll Value
 var scrolled=0.01;
-// var lastScroll;
 
 window.addEventListener("scroll",function(e){
-    // lastScroll=scrolled;
     scrolled = window.pageYOffset || document.documentElement.scrollTop;
     console.log(scrolled)
 })
@@ -18,3 +17,24 @@ document.addEventListener("mousemove",(e)=>{
     cursor.style.left = x + "px";
     cursor.style.top = y + scrolled + "px";
 })
+
+// Light/Dark Mode
+
+let root = document.documentElement; // Getting CSS Variables access
+var dark_switch = document.getElementById("dark_switch");
+var toggleLight=true; // 1 = Light
+dark_switch.addEventListener("change",()=>{
+    toggleLight=!toggleLight;
+    console.log(toggleLight)
+    if(!toggleLight){
+        root.style.setProperty('--background', "#121212");
+        root.style.setProperty('--text_and_details', "#ffffff");
+        root.style.setProperty('--section_background', "#161616");
+    }
+    else if(toggleLight){
+        root.style.setProperty('--background', "#ffffff");
+        root.style.setProperty('--text_and_details', "#121212");
+        root.style.setProperty('--section_background', "#e4e4e4");
+    }
+})
+
